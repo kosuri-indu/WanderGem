2ba5764f23090ca278bdcc0e89e4d2856074cc0c
SriramNamratha <namrathasriram28@gmail.com>
2025-04-04 18:57:22 +0530

    added color palette

---
+++ frontend/lib/data/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF006D77); // Deep Teal
+const Color secondaryColor = Color(0xFF444444);
+const Color backgroundColor = Color(0xFFFEF10D);
+const Color textColor = Colors.black;
+const Color buttonTextColor = Colors.white;
+const Color dividerColor = Colors.grey;
